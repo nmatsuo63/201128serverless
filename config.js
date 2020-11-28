786003f41a7c309a9f2b16fc275c6e3d651020ef
Nao <nmatsuo.63@gmail.com>
2020-11-28 19:52:59 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+﻿// TODO: 作成したユーザープールおよびIDプールのIDに書き換えてください
+const CognitoConfig = {
+  region: 'ap-northeast-1',
+
+  // User Pool
+  userPoolId: 'ap-northeast-1_YJa2Wvq6b',
+  appClientId: 'encfrussrrqa1r32sp6s22tna',
+
+  // Federated Identity
+  identityPoolId: 'ap-northeast-1:9128db0a-b5ad-444f-bc3d-f0691ff56fd6',
+};
